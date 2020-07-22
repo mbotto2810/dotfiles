ad607af9e492e9d6e673ff01cc7c7436e579acd0
Maruan <maruanbakriottoni@gmail.com>
2020-07-21 21:32:44 -0300

    auto

--- .local/src/dwmblocks/config.h
+++ .local/src/dwmblocks/config.h
@@ -1,21 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+//	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"music",	1,	11},
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+//	{"",	"music",	1,	11},
+//	{"",	"pacpackages",	0,	8},
+//	{"",	"news",		0,	6},
 //	{"",	"crypto",	0,	13},
-	{"",	"torrent",	20,	7},
-	{"",	"memory",	10,	14},
-	{"",	"cpu",		10,	13},
-	{"",	"moonphase",	18000,	5},
-	{"",	"weather",	18000,	5},
+//	{"",	"torrent",	20,	7},
+//	{"",	"memory",	10,	14},
+//	{"",	"cpu",		10,	13},
+//	{"",	"moonphase",	18000,	5},
+//	{"",	"weather",	18000,	5},
 	{"",	"mailbox",	180,	12},
 //	{"",	"nettraf",	1,	16},
-	{"",	"volume",	0,	10},
-	{"",	"battery | tr \'\n\' \' \'",	5,	3},
-	{"",	"internet",	5,	4},
+//	{"",	"volume",	0,	10},
+//	{"",	"battery | tr \'\n\' \' \'",	5,	3},
+//	{"",	"internet",	5,	4},
 	{"",	"clock",	60,	1},
 //	{"",	"help-icon",	0,	15},
 };
