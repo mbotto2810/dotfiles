a409ad65acc433d3d580bc35a9db1fc02a0ae0c5
Maruan <maruanbakriottoni@gmail.com>
2020-07-31 20:04:27 -0300

    auto

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 3;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 3;       /* vert inner gap between windows */
+static const unsigned int gappih    = 3;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 3;        /* vert inner gap between windows */
 static const unsigned int gappoh    = 3;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 3;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 3;        /* vert outer gap between windows and screen edge */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 0;        /* 0 means no bar */
