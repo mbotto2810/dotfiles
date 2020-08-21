d356087f25329a5e4ac87e032a1f3b3a3fe64e55
Maruan <maruanbakriottoni@gmail.com>
2020-08-21 12:32:03 -0300

    auto

--- .local/src/dwmblocks/config.h
+++ .local/src/dwmblocks/config.h
@@ -1,27 +1,57 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 //	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	{"",    "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+    {"",    "space.sh"},
+
 	{"",	"music",	1,	11},
+    {"",    "space.sh"},
+
 //	{"",	"pacpackages",	0,	8},
+
 //	{"",	"news",		0,	6},
+
 //	{"",	"crypto",	0,	13},
+
 	{"",	"torrent",	20,	7},
+    {"",    "space.sh"},
+
 //	{"",	"memory",	10,	14},
+
 //	{"",	"cpu",		10,	13},
+
 //	{"",	"moonphase",	18000,	5},
+
 //	{"",	"weather",	18000,	5},
+
 	{"",	"mailbox",	180,	12},
+    {"",    "space.sh"},
+
 //	{"",	"nettraf",	1,	16},
+
 	{"",	"volume",	0,	10},
+    {"",    "space.sh"},
+
 	{"",	"battery | tr \'\n\' \' \'",	5,	3},
+    {"",    "space.sh"},
+
 	{"",	"internet",	5,	4},
+    {"",    "space.sh"},
+
 	{"",	"clock",	60,	1},
+	{"",    "echo '🐧' "},
+	{"",    "echo '🐧' "},
+	{"",    "echo '🐧' "},
+
+
+
+
+
 //	{"",	"help-icon",	0,	15},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = ' ';
+static char delim = '~';
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
